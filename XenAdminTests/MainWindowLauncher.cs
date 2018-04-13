3009ca86aedc1a21c2cb61c23d483f005aaaf28b
Alexander Schulz <code@schulzalex.de>
2018-04-13 22:57:10 +0200

    Use Nunit and Moq from NuGet; Adapt tests to Nunit 3
    https://github.com/xcp-ng/xenadmin/issues/24 Get Tests running
    
    # Conflicts:
    #	XenAdminTests/UnitTests/Diagnostics/HotFixFactoryTests.cs
    #	XenAdminTests/XenAdminTests.csproj

--- XenAdminTests/MainWindowLauncher.cs
+++ XenAdminTests/MainWindowLauncher.cs
@@ -214,13 +214,13 @@ namespace XenAdminTests
             : base(readOnly, databases)
         { }
 
-        [TestFixtureSetUp]
+        [OneTimeSetUp]
         public void SetUp()
         {
             base._SetUp();
         }
 
-        [TestFixtureTearDown]
+        [OneTimeTearDown]
         public void TearDown()
         {
             base._TearDown();
